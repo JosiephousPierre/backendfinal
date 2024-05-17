0f77b6cd9c7ee8a664a8a41b5b886ad3f9cbddda
Dosdos, Josiephous Pierre G. <142465754+JosiephousPierre@users.noreply.github.com>
2024-05-17 23:13:52 +0800

    final3

--- database.py
+++ database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://thisisclinic:thisispassword@159.65.128.24:3306/uic_clinic"
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root@localhost/uic_clinic" #Ito po yung ginamit kong URL for DB - Jem Pillora
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://thisisclinic:thisispassword@159.65.128.24:3306/uic_clinic"
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root@localhost/uic_clinic" #Ito po yung ginamit kong URL for DB - Jem Pillora
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, echo=True
